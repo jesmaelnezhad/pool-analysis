9730c5a6eb44a95f7d876777aca1e3ef65284425
Jamshid Esmaelnezhad <j.esmaelnezhad@cafebazaar.ir>
2020-06-17 15:27:41 +0430

    Add function for update and read of last_block_no_seen variable

--- data_fetcher/__init__.py
+++ data_fetcher/__init__.py
@@ -4,3 +4,12 @@ logger("data_fetcher").info("Setting up the data fetcher")
 
 LAST_BLOCK_NO_SEEN = None
 # TODO Find the most recent block no seen
+
+
+def update_last_block_no_seen():
+    # TODO read the block no of the most recent block found in the database
+    pass
+
+
+def get_last_block_no_seen():
+    return LAST_BLOCK_NO_SEEN
--- data_fetcher/update.py
+++ data_fetcher/update.py
@@ -1,3 +1,4 @@
+from data_fetcher import get_last_block_no_seen, update_last_block_no_seen
 from utility import logger
 
 
@@ -7,3 +8,6 @@ def update():
     :return: None
     """
     logger("data_fetcher").info("Updating the data from the pool web API")
+    last_block_no = get_last_block_no_seen()
+    # FIXME Update logic
+    update_last_block_no_seen()
